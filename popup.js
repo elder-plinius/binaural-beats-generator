593f5d2c228a5bb73cec5e4658004cf3fb7b82a6
pliny <133052465+elder-plinius@users.noreply.github.com>
2023-08-12 16:10:44 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   let slider = document.getElementById('frequencySlider');
   let display = document.getElementById('frequencyDisplay');
+  let playPauseButton = document.getElementById('togglePlayPause');
 
   chrome.storage.sync.get('frequency', (data) => {
     slider.value = data.frequency;
@@ -11,4 +12,15 @@ document.addEventListener('DOMContentLoaded', () => {
     display.textContent = `Frequency: ${slider.value} Hz`;
     chrome.storage.sync.set({frequency: slider.value});
   };
+
+  playPauseButton.addEventListener('click', function() {
+    // Send a message to the background script to toggle play/pause
+    chrome.runtime.sendMessage({action: 'togglePlayPause'}, (response) => {
+      if (response.status === 'playing') {
+        playPauseButton.textContent = 'Pause';
+      } else {
+        playPauseButton.textContent = 'Play';
+      }
+    });
+  });
 });
