1f93b9b999e3acf0e47bf8e77b843cde61d7078e
pliny <133052465+elder-plinius@users.noreply.github.com>
2023-08-12 17:29:48 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,50 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let frequencySlider = document.getElementById('frequencySlider');
-    let frequencyDisplay = document.getElementById('frequencyDisplay');
-    let deltaSlider = document.getElementById('deltaSlider');
-    let deltaDisplay = document.getElementById('deltaDisplay');
-    let volumeSlider = document.getElementById('volumeSlider');
-    let volumeDisplay = document.getElementById('volumeDisplay');
-    let playPauseButton = document.getElementById('togglePlayPause');
-
-    chrome.storage.sync.get(['frequency', 'delta', 'volume'], (data) => {
-        frequencySlider.value = data.frequency || 440;
-        frequencyDisplay.textContent = `Frequency: ${frequencySlider.value} Hz`;
-        deltaSlider.value = data.delta || 10;
-        deltaDisplay.textContent = `Delta: ${deltaSlider.value} Hz`;
-        volumeSlider.value = data.volume || 0.5;
-        volumeDisplay.textContent = `Volume: ${Math.round(volumeSlider.value * 100)}%`;
-    });
-
-    frequencySlider.oninput = () => {
-        let frequencyValue = frequencySlider.value;
-        let brainwaveCategory;
-
-        if (frequencyValue <= 4) {
-            brainwaveCategory = "Delta (Deep Sleep)";
-        } else if (frequencyValue <= 8) {
-            brainwaveCategory = "Theta (Meditation)";
-        } else if (frequencyValue <= 14) {
-            brainwaveCategory = "Alpha (Relaxation)";
-        } else if (frequencyValue <= 30) {
-            brainwaveCategory = "Beta (Focus)";
-        } else {
-            brainwaveCategory = "Gamma (Insight)";
-        }
-
-        frequencyDisplay.textContent = `Frequency: ${frequencyValue} Hz - ${brainwaveCategory}`;
-        chrome.storage.sync.set({frequency: frequencyValue});
-    };
-
-    deltaSlider.oninput = () => {
-        deltaDisplay.textContent = `Delta: ${deltaSlider.value} Hz`;
-        chrome.storage.sync.set({delta: deltaSlider.value});
-    };
-
-    volumeSlider.oninput = () => {
-        volumeDisplay.textContent = `Volume: ${Math.round(volumeSlider.value * 100)}%`;
-        chrome.storage.sync.set({volume: volumeSlider.value});
-    };
+let frequencySlider = document.getElementById('frequencySlider');
+let deltaSlider = document.getElementById('deltaSlider');
+let volumeSlider = document.getElementById('volumeSlider');
+let playPauseButton = document.getElementById('togglePlayPause');
 
 playPauseButton.addEventListener('click', function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
