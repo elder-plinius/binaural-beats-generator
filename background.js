c8694942de7639a5522746845d4f3c617c3b90a1
pliny <133052465+elder-plinius@users.noreply.github.com>
2023-08-23 07:34:37 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,53 +1,56 @@
-let audioContext;
-let oscillatorLeft;
-let oscillatorRight;
-let gainNode;
+let audioContext = new (window.AudioContext || window.webkitAudioContext)();
+let oscillatorLeft, oscillatorRight, gainNode;
 let isPlaying = false;
 
-function startBinauralBeats(frequency, delta, volume) {
-    if (audioContext) {
-        audioContext.close();
-    }
+function startBinauralBeats() {
+  oscillatorLeft = audioContext.createOscillator();
+  oscillatorRight = audioContext.createOscillator();
+  gainNode = audioContext.createGain();
 
-    audioContext = new AudioContext();
+  oscillatorLeft.type = 'sine';
+  oscillatorRight.type = 'sine';
 
-    oscillatorLeft = audioContext.createOscillator();
-    oscillatorRight = audioContext.createOscillator();
-    gainNode = audioContext.createGain();
+  updateFrequencies();
+  updateVolume();
 
-    oscillatorLeft.frequency.setValueAtTime(frequency, audioContext.currentTime);
-    oscillatorRight.frequency.setValueAtTime(frequency + delta, audioContext.currentTime);
+  oscillatorLeft.connect(gainNode);
+  oscillatorRight.connect(gainNode);
+  gainNode.connect(audioContext.destination);
 
-    gainNode.gain.setValueAtTime(volume, audioContext.currentTime);
+  oscillatorLeft.start();
+  oscillatorRight.start();
 
-    oscillatorLeft.connect(gainNode);
-    oscillatorRight.connect(gainNode);
-    gainNode.connect(audioContext.destination);
-
-    oscillatorLeft.start();
-    oscillatorRight.start();
-
-    isPlaying = true;
+  isPlaying = true;
 }
 
 function stopBinauralBeats() {
-    if (oscillatorLeft) {
-        oscillatorLeft.stop();
-    }
-    if (oscillatorRight) {
-        oscillatorRight.stop();
-    }
-    isPlaying = false;
+  oscillatorLeft.stop();
+  oscillatorRight.stop();
+  isPlaying = false;
+}
+
+function updateFrequencies(frequency, delta) {
+  oscillatorLeft.frequency.setValueAtTime(frequency, audioContext.currentTime);
+  oscillatorRight.frequency.setValueAtTime(frequency + delta, audioContext.currentTime);
+}
+
+function updateVolume(volume) {
+  gainNode.gain.setValueAtTime(volume, audioContext.currentTime);
 }
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'togglePlayPause') {
-        if (isPlaying) {
-            stopBinauralBeats();
-            sendResponse({ status: 'paused' });
-        } else {
-            startBinauralBeats(message.frequency, message.delta, message.volume);
-            sendResponse({ status: 'playing' });
-        }
-    }
+  switch (message.action) {
+    case 'start':
+      startBinauralBeats();
+      break;
+    case 'stop':
+      stopBinauralBeats();
+      break;
+    case 'updateFrequencies':
+      updateFrequencies(message.frequency, message.delta);
+      break;
+    case 'updateVolume':
+      updateVolume(message.volume);
+      break;
+  }
 });
